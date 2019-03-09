d32b562e68ad92c2d4edfa9973e231fcf2fc1772
petdomaa100 <45827265+petdomaa100@users.noreply.github.com>
2019-03-09 12:44:48 +0400

    Update reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -13,43 +13,61 @@ module.exports.run = async (bot, message, args, prefix) => {
         return;
     }
 
-    function Reboot() {
-        try {
-        } catch (err) {
-            message.channel.send(`Valami elbaszódott a commandok újratöltsése közben.`);
-            return;
-        }    
-    }
-
     if(bot.voiceConnection) {
         message.guild.voiceConnection.disconnect();
     }
 
+    const CHANNEL = message.guild.channels.find(channel => channel.id == '526037223196590080');
+
     var loadMessage = await message.channel.send('*Commandok előkészítése...*');
 
-    await fs.readdir('../code/commands', async(err, files) => {
+    await fs.readdir(__dirname, async(err, files) => {
         if(err) console.log(err);
 
         var index = 0;
-        var jsfile = files.filter(f => f.split('.').pop() === 'js');
+        var jsfile = await files.filter(f => f.split('.').pop() === 'js');
         
-        jsfile.forEach(async (f) => {
-            delete require.cache[require.resolve(`./${f}`)];
-
-            bot.commands.delete(f);
-
-            const props = require(`./${f}`);
-            
-            await bot.commands.set(props.help.name, props);
-            index++;
+        try {
+            await jsfile.forEach(async (f) => {
+                delete require.cache[require.resolve(`./${f}`)];
     
-            const eltelt = 'l'.repeat(index);
-            const maradek = '.'.repeat(jsfile.length - eltelt.length);
-            
-            szazalek = eltelt + maradek;
+                bot.commands.delete(f);
+    
+                const props = require(`./${f}`);
+                
+                await bot.commands.set(props.help.name, props);
+                index++;
+        
+                const eltelt = 'l'.repeat(index);
+                const maradek = '.'.repeat(jsfile.length - eltelt.length);
+                
+                szazalek = eltelt + maradek;
+    
+                loadMessage.edit(`__Reloading:__ \`${f}\` \n[${szazalek}] (${index}/${jsfile.length})`);
+            });    
+        } catch (err) {
+            message.channel.send('Error történt! \n\n' + err.message);
+            return;
+        }
+        
+        await loadMessage.edit('**Commandok újratöltve!**');
 
-            loadMessage.edit(`__Reloading:__ \`${f}\` \n[${szazalek}] (${index}/${jsfile.length})`);
-        });
+        await Sleep(2000);
+
+        await loadMessage.edit('**Újraindítom magam...**').then(() => {
+            bot.destroy().then(() => bot.login(process.env.TOKEN)).then(() => {
+                loadMessage.edit('**Újraindultam.**');
+        
+                let rebootOutput = new Discord.RichEmbed()
+                    .setTitle('Moór újraindult!')
+                    .setDescription(`${message.author.username} újraindította Moór-t.`)
+                    .setColor('#FF9900')
+                    .setTimestamp()
+                CHANNEL.send(rebootOutput);
+    
+                loadMessage.delete(4000);
+            });
+        });  
     });
 }   
 
