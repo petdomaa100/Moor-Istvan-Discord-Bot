0a4bc77f985713f3f711e1d4e8c6fb601c271b2c
petdomaa100 <45827265+petdomaa100@users.noreply.github.com>
2019-10-31 08:59:17 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ function loadCommands() {
             return;
         }
     
-        jsfile.forEach((f, i) => {
+        jsfile.forEach((f) => {
             let props = require(`./commands/${f}`);
             bot.commands.set(props.help.name, props);
-            props.help.aliases.forEach(alias => {
-                bot.aliases.set(alias, props.help.name, props)
-            });
+
+            if(props.help.aliases) {
+                props.help.aliases.forEach(alias => {
+                    bot.aliases.set(alias, props.help.name, props);
+                });
+            }
         });
 
         console.log(`${jsfile.length} commands loaded!`);
@@ -35,6 +38,11 @@ loadCommands();
 global.nowPlaying = null;
 global.queue = [];
 global.blacklist = [];
+global.wallOfShame = [];
+
+const undoritoKepek = ['https://i.ytimg.com/vi/YubEjXt1Bf4/maxresdefault.jpg', 'https://i.ytimg.com/vi/p3gZi62iefo/maxresdefault.jpg', 'https://i.pinimg.com/originals/23/6a/42/236a42486c6b097c6fc92e0476984180.jpg', 'https://i.ytimg.com/vi/tOJlVPW3PGw/maxresdefault.jpg', 'https://i.ytimg.com/vi/jxZSILBrowA/maxresdefault.jpg', 'https://i.ytimg.com/vi/IvX2_7IM9gs/maxresdefault.jpg'];
+
+const outputMessagesJSON = require('./data/outputMessages.json');
 
 global.Sleep = function(millisec) {
     var start = new Date().getTime();
@@ -45,9 +53,39 @@ global.Sleep = function(millisec) {
     }
 }
 
-const undoritoKepek = ['https://i.ytimg.com/vi/YubEjXt1Bf4/maxresdefault.jpg', 'https://i.ytimg.com/vi/p3gZi62iefo/maxresdefault.jpg', 'https://i.pinimg.com/originals/23/6a/42/236a42486c6b097c6fc92e0476984180.jpg', 'https://i.ytimg.com/vi/tOJlVPW3PGw/maxresdefault.jpg', 'https://i.ytimg.com/vi/jxZSILBrowA/maxresdefault.jpg', 'https://i.ytimg.com/vi/IvX2_7IM9gs/maxresdefault.jpg']
+global.Separate = function(number, sepSign) {
+    number = number.toString().split('').reverse().join('');
 
-global.death = function(aldozat, i) {
+    szamReturn = [];
+
+    for (let i = 0; i < number.length; i++) {
+        const szam = number[i].toString();
+
+        if((i + 1) % 3 === 0 && i != number.length - 1) {
+            szamReturn.push(sepSign + szam);
+        } else {
+            szamReturn.push(szam);
+        }
+    }
+
+    return szamReturn.reverse().join('').toString();
+}
+
+global.SzazalekBar = function(barLength, osszes, ebbolMegvan) {
+    var barLength = parseInt(barLength); //100
+    var osszes = parseInt(osszes); //30
+    var ebbolMegvan = parseInt(ebbolMegvan); //25
+    var egyBar = osszes / barLength; //0.3
+
+    const megvan = parseInt( Math.floor(ebbolMegvan / egyBar) ); //10
+    const hianyzik = parseInt(barLength - megvan); //90
+
+    if(hianyzik < 0) throw 'Overflow';
+    
+    return `${megvan}__${hianyzik}`;
+}
+
+global.Death = function(aldozat, i) {
     const USER = bot.users.find(x => x.discriminator == aldozat.user.split('#')[1]);
     const NUMBER = Math.floor(Math.random() * undoritoKepek.length);
 
@@ -65,13 +103,17 @@ global.death = function(aldozat, i) {
             server.member(USER).kick(USER.reason);
             
             blacklist.splice(i, 1);
-            console.log(blacklist);
         }
     });
 }
 
+global.outputMessageRandomiser = function(msg) {
+    return outputMessagesJSON[msg][ Math.floor(Math.random() * outputMessagesJSON[msg].length) ];
+}
+
 bot.on("message", async (message) => {
     if(message.channel.type === 'dm') return;
+    if(bot.user.presence.status == 'dnd') return;  
 
     const prefixJSON = require('./data/prefix.json');
     var prefix;
@@ -96,7 +138,7 @@ bot.on("message", async (message) => {
     const cmd = messageArray[0].toLowerCase();
     const args = messageArray.slice(1);
     const commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
-    if(commandfile) commandfile.run(bot, message, args, prefix);
+    if(commandfile) commandfile.run(bot, message, args, prefix, prefixJSON, outputMessagesJSON);
 });
 
-bot.login(process.env.TOKEN);
+bot.login(botconfig.token);
